4faf1b937a458650bc785fbaca42b8b8e2c2040d
madmoohs <madmoohs@gmail.com>
2026-06-02 13:21:09 +0800

    ran tf format

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce12-tfstate-bucket" # Change this
-    key    = "muhs-assignment3.2-tfstate"  # Change this
+    bucket = "sctp-ce12-tfstate-bucket"   # Change this
+    key    = "muhs-assignment3.2-tfstate" # Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix =   muhs-assignment3.2
+  bucket_prefix = muhs-assignment3.2
 }
