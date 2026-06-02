6fb267a66d110d82c0f0ac3b161ff1c12864c9b4
madmoohs <madmoohs@gmail.com>
2026-06-02 17:28:38 +0800

    Add TF version constraints

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
-    bucket = "sctp-ce12-tfstate-bucket"   # Change this
-    key    = "muhs-assignment3.2-tfstate" # Change this
+    bucket = "sctp-ce12-tfstate-bucket"
+    key    = "muhs-assignment3.2-tfstate"
     region = "ap-southeast-1"
   }
 }
 
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "muhs-assignment3-2"
+  bucket_prefix = "muhs-assignment3-2-"
 }
